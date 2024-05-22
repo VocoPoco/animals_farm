6ca4cbe5bc72b3aeadb48a795541354867d599a2
VocoPoco <VocoPoco@email.com>
2024-05-22 17:52:48 +0300

    feat: init java maven project

---
+++ animals_farm/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Alt+F5 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing F9.
+            System.out.println("i = " + i);
+        }
+    }
+}
