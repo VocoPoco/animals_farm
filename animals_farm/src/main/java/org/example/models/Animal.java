c4e46c93c9f44b719ddc754d46963d253e25194a
VocoPoco <VocoPoco@email.com>
2024-05-22 18:30:53 +0300

    feat: made abstract model for Animal

--- animals_farm/src/main/java/org/example/models/Animal.java
+++ animals_farm/src/main/java/org/example/models/Animal.java
@@ -1,9 +1,31 @@
 package org.example.models;
 
-public class Animal {
+abstract class Animal {
+    private final int lifespan;
+    private final int chanceOfGettingSick;
+    private final int foodConsumption;
+    private final int foodProduction;
 
-
-    public Animal() {
+    public Animal(int lifespan, int chanceOfGettingSick, int foodConsumption, int foodProduction ) {
+        this.lifespan = lifespan;
+        this.chanceOfGettingSick = chanceOfGettingSick;
+        this.foodConsumption = foodConsumption;
+        this.foodProduction = foodProduction;
     }
-    
+
+    public int getLifespan() {
+        return lifespan;
+    }
+
+    public int getChanceOfGettingSick() {
+        return chanceOfGettingSick;
+    }
+
+    public int getFoodConsumption() {
+        return foodConsumption;
+    }
+
+    public int getFoodProduction() {
+        return foodProduction;
+    }
 }
